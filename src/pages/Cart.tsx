d6de2dccb84866a13901e996f07757922b9fbe27
Yasin Hirani <yasin.hirani@1rivet.local>
2023-02-27 19:01:03 +0530

    Fix : some ui issues in table

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -84,62 +84,64 @@ const Cart = () => {
   }, [cartItems]);
 
   return (
-    <div className="flex-grow h-full w-full max-w-baseWidth mx-auto px-6 md:px-12 py-6 mt-20">
+    <div className="flex-grow h-full w-full max-w-baseWidth mx-auto px-6 md:px-12 py-6 mt-20 overflow-x-hidden">
       <h4 className="text-2xl font-bold">
         Your Shopping Cart {cartItems.length === 0 ? "is empty." : ""}
       </h4>
-      {cartItems.length > 0 && (
-        <table className="w-full mt-10">
-          <thead>
-            <tr>
-              <td className="font-bold thead">Product</td>
-              <td className="font-bold thead">Item Name</td>
-              <td className="font-bold thead">Price</td>
-              <td className="font-bold thead">Quantity</td>
-              <td className="font-bold thead">Total</td>
-              <td className="font-bold thead">Remove</td>
-            </tr>
-          </thead>
-          <tbody>
-            {cartItems.map((item) => (
-              <tr key={Math.random()}>
-                <td className="tbody align-top">
-                  <figure>
-                    <img src={item.itemImage} alt="" className="w-36 h-24" />
-                  </figure>
-                </td>
-                <td className="tbody align-top">{item.itemName}</td>
-                <td className="tbody align-top">RS.{item.price}</td>
-                <td className="tbody align-top">
-                  <div className="flex items-center space-x-5">
-                    <button
-                      className="font-semibold text-xl disabled:text-gray-400 disabled:cursor-not-allowed"
-                      disabled={item.quantity === 1}
-                      onClick={() => decreaseQuantity(item.itemId)}
-                    >
-                      -
-                    </button>
-                    <span>{item.quantity}</span>
-                    <button
-                      className="font-semibold text-xl disabled:text-gray-400 disabled:cursor-not-allowed"
-                      disabled={item.quantity > 4}
-                      onClick={() => increaseQuantity(item.itemId)}
-                    >
-                      +
-                    </button>
-                  </div>
-                </td>
-                <td className="tbody align-top">{item.total}</td>
-                <td className="tbody align-top">
-                  <button onClick={() => removeItem(item.itemId)}>
-                    Remove
-                  </button>
-                </td>
+      <div className="overflow-x-auto">
+        {cartItems.length > 0 && (
+          <table className="w-full mt-10 cart-table">
+            <thead>
+              <tr>
+                <td className="font-bold thead">Product</td>
+                <td className="font-bold thead">Item Name</td>
+                <td className="font-bold thead">Price</td>
+                <td className="font-bold thead">Quantity</td>
+                <td className="font-bold thead">Total</td>
+                <td className="font-bold thead">Remove</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            </thead>
+            <tbody>
+              {cartItems.map((item) => (
+                <tr key={Math.random()}>
+                  <td className="tbody">
+                    <figure>
+                      <img src={item.itemImage} alt="" className="w-36 h-24 min-w-[9rem]" />
+                    </figure>
+                  </td>
+                  <td className="tbody">{item.itemName}</td>
+                  <td className="tbody">RS.{item.price}</td>
+                  <td className="tbody">
+                    <div className="flex items-center space-x-5">
+                      <button
+                        className="font-semibold text-xl disabled:text-gray-400 disabled:cursor-not-allowed"
+                        disabled={item.quantity === 1}
+                        onClick={() => decreaseQuantity(item.itemId)}
+                      >
+                        -
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button
+                        className="font-semibold text-xl disabled:text-gray-400 disabled:cursor-not-allowed"
+                        disabled={item.quantity > 4}
+                        onClick={() => increaseQuantity(item.itemId)}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </td>
+                  <td className="tbody">{item.total}</td>
+                  <td className="tbody">
+                    <button onClick={() => removeItem(item.itemId)}>
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
       {cartItems.length > 0 && (
         <div className="mt-10 flex justify-end">
           <div className="bg-primary p-4 rounded-lg w-72 flex flex-col">
